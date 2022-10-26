b822c5ffee0fa6edc552b2703a4481a1f48f3f98
thinkingmore <peterkevin823@gmail.comm>
2022-10-26 12:27:01 +0600

    added a header to the course details page

--- src/Pages/Courses/Course.js
+++ src/Pages/Courses/Course.js
@@ -1,27 +1,49 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
-import { Button } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
+import { FaArrowCircleRight } from 'react-icons/fa';
 
 const Course = () => {
     const course = useLoaderData();
     console.log(course);
     return (
-        <Card style={{width:'30rem'}}>
-            <Card.Img variant="top" src={course.img_url} />
-            <Card.Body>
-            <Card.Title>{course.course_title}</Card.Title>
-            <Card.Text>
-               {course.details}
-            </Card.Text>
-            <div className='d-flex justify-content-between mb-4 fw-bold'>
-                <span>Price:  {course.price}</span>
-                <span>Rating: {course.rating}</span>
-                <span>Review: {course.review}</span>
-            </div>
-            <Button style={{width:'10rem'}} variant="primary">Buy</Button>
-            </Card.Body>
-       </Card>
+        <Container>
+            <Card className='my-4'>
+                <Card.Header>Welcome to our course</Card.Header>
+                <Card.Body>
+                    <Card.Title>Before you purchase this course please read the following:</Card.Title>
+                    <Card.Text>
+                        <ol>
+                            <li>This course gives your access to course materials and videos for personal use.
+                                You can not use those materials for public distribution.
+                                You can not share the course video or materials</li>
+                            <li>You can not any parts of course video or materials and share those 
+                                on social media platforms</li>
+                            <li>You can download the course videos or materials for preservation.</li>
+                            <li>You can not sell or transfer your access too this course.</li>    
+                        </ol>
+                    </Card.Text>
+                    <Button variant="primary">Download <FaArrowCircleRight/></Button>
+                </Card.Body>
+            </Card>
+            <Card style={{width:'30rem'}}>
+                <Card.Img variant="top" src={course.img_url} />
+                <Card.Body>
+                <Card.Title>{course.course_title}</Card.Title>
+                <Card.Text>
+                {course.details}
+                </Card.Text>
+                <div className='d-flex justify-content-between mb-4 fw-bold'>
+                    <span>Price:  {course.price}</span>
+                    <span>Rating: {course.rating}</span>
+                    <span>Review: {course.review}</span>
+                </div>
+                <Button style={{width:'10rem'}} variant="primary">Buy</Button>
+                </Card.Body>
+            </Card>
+        </Container>
+        
     );
 };
 
