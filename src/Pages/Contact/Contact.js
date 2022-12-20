ba29852b7148a498ca864e282acad5235a2137eb
thinkingmore <peterkevin823@gmail.comm>
2022-12-20 21:32:32 +0600

    added a contact form to the contact page

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(`${process.env.REACT_APP_EJ_SID}`, `${process.env.REACT_APP_EJ_TID}`, form.current, `${process.env.REACT_APP_EJ_PK}` )
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
     return (
-        <div>
-            <h3>This is contact</h3>
+        <div id="contact" className='mx-auto' style={{maxWidth:"40rem"}}>
+            <h2 className='my-6 text-center'> Contact Us</h2>
+            {/* <Jotform src="https://form.jotform.com/223433440146447"></Jotform> */}
+            <form ref={form} onSubmit={sendEmail}>
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlInput1"  className="form-label">Name</label>
+                    <input type="text" name="user_name" className="form-control"  placeholder="Name"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlInput1"  className="form-label">Email</label>
+                    <input type="email" name="user_email"  className="form-control"  placeholder="Email"/>
+                </div>
+                <div className="mb-3">
+                    <textarea className="form-control" name="message"  rows="3"/>
+                </div>
+                <button type="submit" className="btn btn-primary mb-3 w-100">Submit</button>
+            </form>       
         </div>
     );
 };
